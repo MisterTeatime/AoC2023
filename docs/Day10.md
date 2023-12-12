e37ebf3174d078340e2695656d6801b8bcb8e7e8
MisterTeatime <j.teatime@gmail.com>
2023-12-12 03:27:42 +0100

    Tag 10 WIP

---
+++ docs/Day10.md
@@ -0,0 +1,5 @@
+# [Tag 10: Pipe Maze](https://adventofcode.com/2023/day/10)
+
+## Teil 1
+
+## Teil 2
---
+++ src/Day10.kt
@@ -0,0 +1,22 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day10_test")
+    val resultPart1 = part1(testInput)
+    println("Test Part 1: $resultPart1")
+    check(part1(testInput) == 1)
+    val resultPart2 = part2(testInput)
+    println("Test Part 2: $resultPart2")
+    check(part2(testInput) == 1)
+
+    val input = readInput("Day10")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
