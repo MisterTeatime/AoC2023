6add64dc61744838038e4a3570265500e3c0d02b
MisterTeatime <j.teatime@gmail.com>
2023-12-19 17:44:48 +0100

    Tag 17 WIP

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,5 +1,13 @@
+import java.awt.Point
+
 fun main() {
     fun part1(input: List<String>): Int {
+        val area = Area(input.map { row -> row.map { it.toString().toInt()}.toMutableList()})
+
+
+
+
+
         return input.size
     }
 
@@ -20,4 +28,22 @@ fun main() {
     val input = readInput("Day17")
     println("Part 1: ${part1(input)}")
 //    println("Part 2: ${part2(input)}")
+}
+
+class TrolleyPathFinder (val area: Area<Int>, private val start: Point2D, private val goal: Point2D){
+    private val openList = mutableListOf(start)
+    private val closedList = mutableListOf<Point2D>()
+
+}
+
+class TrolleyCost {
+    private val costs = mutableMapOf<Point2D, Pair<Point2D, Int>>()
+
+    fun setCost(cell: Point2D, parent: Point2D, cost: Int) {
+        costs[cell] = parent to cost
+    }
+
+    fun getCost(cell: Point2D) = costs[cell]?.second ?: 0
+
+    fun getParent(cell: Point2D) = costs[cell]?.first
 }
