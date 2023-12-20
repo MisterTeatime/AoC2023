985291813ad78263460887f54240f08915ff0081
MisterTeatime <j.teatime@gmail.com>
2023-12-21 00:26:29 +0100

    Tag 20 vorbereitet

---
+++ docs/Day20.md
@@ -0,0 +1,5 @@
+# [Tag 20: Pulse Propagation](https://adventofcode.com/2023/day/20)
+
+## Teil 1
+
+## Teil 2
---
+++ src/Day20.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day20_test")
+    val resultPart1 = part1(testInput)
+    println("Test Part 1: $resultPart1")
+    check(resultPart1 == 11687500)
+
+//    val resultPart2 = part2(testInput)
+//    println("Test Part 2: $resultPart2")
+//    check(resultPart2 == 1)
+
+    val input = readInput("Day20")
+    println("Part 1: ${part1(input)}")
+//    println("Part 2: ${part2(input)}")
+}
